c02d3eedc037956958f505f4c3cadfcc30cf93a2
shrudex <codeshubh@gmail.com>
2023-10-27 02:26:35 +0530

    🧑🏻‍🏭events page 1

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,15 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import EventsCard from "./components/EventsCard";
 
 const Events = () => {
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Input value:", inputValue);
+  };
+
+  const events = [
+    {
+      event: "PIXELVISION",
+      org: "MIT-GPT-WORKSHOP",
+      desc: "AB3 18 Oct 2023 Embark on a captivating voyage into the realm of computer vision with PixelVision, an OpenCV Workshop. Learn the technique of image processing through a series of hand-on activities and captivating demonstrations in an insightful session.",
+      mode: "OFFLINE",
+    },
+    {
+      event: "GITCRAFT",
+      org: "FOSSIFY-WORKSHOP",
+      desc: "AB3 104 16 Oct 2023 Building Together in Open Source: Want to learn Git, Github , and build your first open-source webpage? This workshop is for you!",
+      mode: "OFFLINE",
+    },
+    {
+      event: "ORK WORKSHOP & VIRTUAL ROCKET BUILDING COMPETION",
+      org: "ESCAPE VELOCITY-WORKSHOP",
+      desc: "Ab3-103 15 Oct 2023 The focus of the event is centered on utilizing the open-source software named open rocket, enabling participants to construct their model rockets from the ground up and subsequently simulate their behavior.",
+      mode: "OFFLINE",
+    },
+    {
+      event: "MICROSOFT FLIGHT SIMULATOR",
+      org: "AEROVERSE-WORKSHOP",
+      desc: "AB3-403 16 Oct 2023 Experience the beauty of aviation firsthand with our flight simulator and take to the skies. Calling all aviation enthusiasts.",
+      mode: "OFFLINE",
+    },
+    {
+      event: "FLIGHT MECHANICS WORKSHOP",
+      org: "AEROVERSE-WORKSHOP",
+      desc: "AB3-102 17 Oct 2023 Have you ever wondered how planes actually fly? This workshop offers you the opportunity to dive deep into the wonderful world of air-flight machines. Covering the basics of flight mechanics and aerodynamics, this informative seminar allows you to visualize flight characteristics and essentially understand the science behind flight.",
+      mode: "OFFLINE",
+    },
+    {
+      event: "NOVA - UI/UX WORKSHOP",
+      org: "CODE RED- WORKSHOP",
+      desc: "AB3 18 Oct 2023 Whether you're a designer or a developer, this workshop will enhance your understanding of user needs and help you master the art of designing intuitive, user-friendly interfaces.",
+      mode: "OFFLINE",
+    },
+    {
+      event: "(M.A.D) MOBILE APP DEVELOPMENT WORKSHOP",
+      org: "CODE RED- WORKSHOP",
+      desc: "AB3 18 Oct 2023 Unlock the world of app development in our immersive workshop, designed for students eager to create, innovate and code. Join us to delve into the art of turning ideas into functional mobile applications. Guided by seasoned developers, you'll learn hands-on coding, UI design, and app deployment.",
+      mode: "OFFLINE",
+    },
+    {
+      event: "EVERYTHING OFF-ROAD",
+      org: "KRAFTWAGEN (WORKSHOP)",
+      desc: "Behind MIT Football ground 15 Oct 2023 An interesting workshop demonstrating various domains involved in the manufacturing of automobiles and industry level skill sets. By using preliminary design(CAD) , analysis (CAE), manufacturing process and finally testing of components.",
+      mode: "OFFLINE",
+    },
+  ];
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full ">
       <h1 className="cardo text-6xl text-center mb-2">Events</h1>
-      <div className="events-header flex w-[99%] px-2 mx-auto items-center justify-between bg-red-600">
-        <h3 className="raleway">
-          TechTatva 2023 : <span>Schedule</span>
+      <div className="events-header flex w-[96%] px-4 my-4 mx-auto items-center justify-between rounded-xl bg-[#915EFF]">
+        <h3 className="raleway text-2xl">
+          TechTatva 2023 : <span className="font-semibold ">Schedule</span>
         </h3>
-        <button className="cardo">Events TimeTable</button>
-        <div className="search raleway">
+        <button className="cardo text-3xl font-bold">Events TimeTable</button>
+        <div className="search raleway text-2xl w-1/4 py-1">
           <form>
             <label
               htmlFor="default-search"
@@ -20,7 +81,7 @@ const Events = () => {
             <div className="relative">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg
-                  className="w-4 h-4 text-gray-500  "
+                  className="w-4 h-4 text-[#915EFF]  "
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -38,19 +99,33 @@ const Events = () => {
               <input
                 type="search"
                 id="default-search"
-                className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  "
+                className="block w-full p-4 pl-10 text-sm font-semibold text-black border border-gray-300 rounded-lg bg-gray-50 focus:ring-transparent"
                 placeholder="Search Events..."
-                required
+                value={inputValue}
+                onChange={handleInputChange}
               />
               <button
-                type="submit"
-                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 "
+                onClick={handleSubmit}
+                type="button"
+                className="text-white absolute right-2.5 bottom-2.5 bg-[#915EFF] hover:bg-[#613dae] focus:ring-0 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 "
               >
                 Search
               </button>
             </div>
           </form>
         </div>
+      </div>
+      <div className="events-grid px-8 py-2 grid grid-cols-4 gap-5">
+        {events.map((event, index) => (
+          <div key={index} className="flex flex-col">
+            <EventsCard
+              event={event.event}
+              org={event.org}
+              desc={event.desc}
+              mode={event.mode}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
